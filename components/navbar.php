f7f8fa786efce78b7a8dc76827a07d2c1b94cab7
sitio_web <alexiamtz123@gmail.com>
2022-11-14 21:34:43 -0700

    tarea3

---
+++ components/navbar.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="style/navbar.css">
+
+<nav class="navbar">
+    <a href="/">HOME</a>
+    <a href="/">ABOUT US</a>
+    <a href="/">MENU</a>
+    <a href="/">ORDEN ONLINE</a>
+    <a href="/">CONTACT</a>
+
+</nav>
