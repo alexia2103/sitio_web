8ee36108e1dd4ba702974f7dc95651ab2749d406
sitio_web <alexiamtz123@gmail.com>
2022-11-15 12:30:05 -0700

    tarea3

---
+++ estructura.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style/estilos.css">
+
+<?php include('components/header.php') ?>
+<?php include('components/navbar.php') ?>
+<body>
+    <div class="contenedor_img">
+        <img src="img/cupcakee.png" alt="cupcakee" class="pastelito">
+    </div>
+</body>
